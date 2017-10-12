1d7e708f75b02265a56bfe78b737ad8c46034d49
dictatornarf <dictatornarf@gmail.com>
2017-10-12 00:51:17 -0400

    Removed commented out sections of MoviesController

--- JsDataTables/SampleJsDataTables/Controllers/MoviesController.cs
+++ JsDataTables/SampleJsDataTables/Controllers/MoviesController.cs
@@ -11,7 +11,6 @@ namespace SampleJsDataTables.Controllers
     [Route("api/v1/[controller]")]
     public class MoviesController : Controller
     {
-        // GET api/values
         [HttpGet]
         public IEnumerable<Movie> Get()
         {
@@ -21,46 +20,8 @@ namespace SampleJsDataTables.Controllers
         [Route("search")]
         public JsDataTables.Response<Movie> Search()
         {
-            //string json = "";
-            //using (var sr = new System.IO.StreamReader(Request.Body))
-            //{
-            //    json = sr.ReadToEnd();
-            //}
-
             var request = new JsDataTables.Request(Request.Form);
             return new JsDataTables.Response<Movie>(request, Movie.AllMovies);
         }
-
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
-
-        //[HttpPost]
-        //public JsDataTables.Request ViewMovies()
-        //{
-        //    //Request.
-        //    return null;
-        //}
     }
 }
