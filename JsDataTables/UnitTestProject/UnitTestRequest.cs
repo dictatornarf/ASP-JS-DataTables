277ccee4902b1ff1e07fa164ed8fd433815fa6ea
dictatornarf <dictatornarf@gmail.com>
2017-10-23 20:10:47 -0400

    Refactor Test Classes

--- JsDataTables/UnitTestProject/UnitTestRequest.cs
+++ JsDataTables/UnitTestProject/UnitTestRequest.cs
@@ -8,65 +8,16 @@ namespace UnitTestProject
     [TestClass]
     public class UnitTestRequest
     {
-        public Microsoft.AspNetCore.Http.FormCollection PostValues { get; private set; }
-
         [TestInitialize]
         public void Init()
         {
-            System.Collections.Generic.Dictionary<string, Microsoft.Extensions.Primitives.StringValues> values = new System.Collections.Generic.Dictionary<string, Microsoft.Extensions.Primitives.StringValues>()
-            {
-                ["draw"] = "1",
-                ["columns[0] [data]"] = "phase",
-                ["columns[0] [name]"] = "",
-                ["columns[0] [searchable]"] = "true",
-                ["columns[0] [orderable]"] = "true",
-                ["columns[0] [search] [value]"] = "",
-                ["columns[0] [search] [regex]"] = "false",
-                ["columns[1] [data]"] = "name",
-                ["columns[1] [name]"] = "",
-                ["columns[1] [searchable]"] = "true",
-                ["columns[1] [orderable]"] = "true",
-                ["columns[1] [search] [value]"] = "",
-                ["columns[1] [search] [regex]"] = "false",
-                ["columns[2] [data]"] = "releaseDate",
-                ["columns[2] [name]"] = "",
-                ["columns[2] [searchable]"] = "true",
-                ["columns[2] [orderable]"] = "true",
-                ["columns[2] [search] [value]"] = "",
-                ["columns[2] [search] [regex]"] = "false",
-                ["columns[3] [data]"] = "director",
-                ["columns[3] [name]"] = "",
-                ["columns[3] [searchable]"] = "true",
-                ["columns[3] [orderable]"] = "true",
-                ["columns[3] [search] [value]"] = "",
-                ["columns[3] [search] [regex]"] = "false",
-                ["columns[4] [data]"] = "screenwriter",
-                ["columns[4] [name]"] = "",
-                ["columns[4] [searchable]"] = "true",
-                ["columns[4] [orderable]"] = "true",
-                ["columns[4] [search] [value]"] = "",
-                ["columns[4] [search] [regex]"] = "false",
-                ["columns[5] [data]"] = "producer",
-                ["columns[5] [name]"] = "",
-                ["columns[5] [searchable]"] = "true",
-                ["columns[5] [orderable]"] = "true",
-                ["columns[5] [search] [value]"] = "",
-                ["columns[5] [search] [regex]"] = "false",
-                ["order[0] [column]"] = "2",
-                ["order[0] [dir]"] = "asc",
-                ["start"] = "0",
-                ["length"] = "10",
-                ["search[value]"] = "",
-                ["search[regex]"] = "false"
-            };
 
-            PostValues = new Microsoft.AspNetCore.Http.FormCollection(values);
         }
 
         [TestMethod]
         public void DecodeForm()
         {
-            JsDataTables.Request request = new JsDataTables.Request(PostValues);
+            JsDataTables.Request request = new JsDataTables.Request(TestData.PostValuesInitialState);
 
             Assert.AreEqual(request.Draw, 1);
 
--- JsDataTables/UnitTestProject/UnitTestResponse.cs
+++ JsDataTables/UnitTestProject/UnitTestResponse.cs
@@ -9,12 +9,13 @@ namespace UnitTestProject
         [TestInitialize]
         public void Init()
         {
+            
         }
 
         [TestMethod]
         public void Process()
         {
-
+            JsDataTables.Request request = new JsDataTables.Request(TestData.PostValuesInitialState);
         }
     }
 }
